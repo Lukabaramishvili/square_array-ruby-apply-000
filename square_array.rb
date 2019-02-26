d6f338cdc894b8a2c95f625f98e2190b223c4a4b
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-26 18:03:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-# def square_array(array)
-#   new_array = []
-#   array.each do |ele| 
-#     new_array << ele*ele
-#   end
-#   return new_array
-# end
+def square_array(array)
+  new_array = []
+  array.each do |ele| 
+    new_array << ele*ele
+  end
+  return new_array
+end
 
 # def square_array(array)
 #   new_array = []
@@ -13,9 +13,3 @@
 #   end
 #   return new_array
 # end
-
-def square_array(array)
-  array.collect do |ele| 
-   puts ele*ele
- end
-end
